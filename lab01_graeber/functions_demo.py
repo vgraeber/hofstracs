7d221ac71cb5fbc40ce40a605d10f81da3654934
vivian graeber <h703248488@dhcp-147-4-180-108.dhcp.hofstra.edu>
2023-09-07 13:49:25 -0400

    work

---
+++ lab01_graeber/functions_demo.py
@@ -0,0 +1,13 @@
+# Author: Vivian Graeber
+# Date: 9/7/23
+# Description: Simple functions in python
+
+def compuuteSum(nums):
+  sum = 0
+  for i in nums:
+    sum += i
+  return sum
+
+def main():
+  quiz = []98, 78, 79, 83, 87]
+  return computeSum(quiz)
