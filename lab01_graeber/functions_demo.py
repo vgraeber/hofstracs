f37df22c6ada3e95cc24e6f34440ee68dfd39da9
vivian graeber <h703248488@dhcp-147-4-180-108.dhcp.hofstra.edu>
2023-09-07 13:55:39 -0400

    work

--- lab01_graeber/functions_demo.py
+++ lab01_graeber/functions_demo.py
@@ -2,14 +2,14 @@
 # Date: 9/7/23
 # Description: Simple functions in python
 
-def compuuteSum(nums):
+def computeSum(nums):
   sum = 0
   for i in nums:
     sum += i
   return sum
 
 def main():
-  quiz = []98, 78, 79, 83, 87]
-  print("Sum: ",computeSum(quiz))
+  quiz = [98, 78, 79, 83, 87]
+  print("Sum:",computeSum(quiz))
 
 main()
