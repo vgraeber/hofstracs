ed5c179354113dfb08313bb37b8cd8d8bd5acde0
Vivian Graeber <vgraeber@gmail.com>
2023-09-14 23:35:37 -0400

    work

--- csc15/lab01_graeber/twoscomplement.py
+++ csc15/lab01_graeber/twoscomplement.py
@@ -1,9 +1,10 @@
 # Author: Vivian Graeber
-# Date Created: 9/11/23
+# Date Created: 9/12/23
 # Current Date: 9/14/23
-# Description: Conversion functions for binary and decimal numbers
+# Description: Conversion function for Twos Complement in addition to conversion functions for binary and decimal
 
 def convertDecToBin(n):
+  if (n < 0): return toTwosComp(n)
   binN = []
   while (n != 0):
     binN.append(n % 2)
@@ -26,11 +27,27 @@ def convertBinToDec(n):
 def checkNum(num):
   cont = False
   while (not cont):
-    if (isinstance(num, int) and (num >= 0)): cont = True
+    if isinstance(num, int): cont = True
     else:
-      print("Error: Invalid input. Please enter a whole number.")
+      print("Error: Invalid input. Please enter an integer.")
       num = int(input("Please enter the number you wish to convert to decimal: "))
   return num
+
+def toTwosComp(n):
+  twosCompN = convertDecToBin(abs(n))
+  for i in range(len(twosCompN)):
+    if (twosCompN[i] == 1): twosCompN[i] = 0
+    else: twosCompN[i] = 1
+  added = False
+  i = -1
+  while (not added):
+    if (twosCompN[i] == 1):
+      twosCompN[i] = 0
+      i -= 1
+    else:
+      twosCompN[i] = 1
+      added = True
+  return twosCompN
 
 def main():
   num = int(input("Please enter the number you wish to convert to binary: "))
