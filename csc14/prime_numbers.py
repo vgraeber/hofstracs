72608e4330ba2bbc355ef0efa3a4fee09e5056d5
Vivian Graeber <vgraeber@gmail.com>
2023-09-18 11:20:32 -0400

    work

--- csc14/prime_numbers.py
+++ csc14/prime_numbers.py
@@ -2,15 +2,12 @@ import math
 
 def isPrime(n):
   l = math.sqrt(n)
-  if ((l % 1) == 0):
-    return False
+  if ((l % 1) == 0): return False
   l = math.floor(l)
-  if ((n % 2) == 0):
-    return False
+  if ((n % 2) == 0): return False
   for i in range(3, l, 2):
     if (isPrime(i)):
-      if ((n % i) == 0):
-        return False
+      if ((n % i) == 0): return False
   return True
 
 print("167:",isPrime(167))
