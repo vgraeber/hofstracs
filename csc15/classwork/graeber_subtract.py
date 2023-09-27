fd36250a6e5a2c5e85554a56fe8e595bfd8472d9
Vivian Graeber <vgraeber@gmail.com>
2023-09-27 12:44:11 -0400

    work

---
+++ csc15/classwork/graeber_subtract.py
@@ -0,0 +1,26 @@
+# Author: Vivian Graeber
+# Date: 09/27/23
+# Descripton: Subtraction test
+
+import random
+
+Lnum = random.randint(0, 9)
+Snum = random.randint(0, 9)
+
+if (Lnum < Snum): Lnum, Snum = Snum, Lnum
+ques = "What is " + str(Lnum) + " - " + str(Snum) + '?'
+answer = Lnum - Snum
+
+def checkAns(ans):
+  while (ans != answer):
+    print("Wrong answer. Please try again.", ques)
+    ans = int(input("Answer: "))
+  return ans
+
+def main():
+  print(ques)
+  ans = int(input("Answer: "))
+  checkAns(ans)
+  print("You got it!")
+
+main()
