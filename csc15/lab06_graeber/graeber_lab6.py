7f107d9bb361c75a97ed6fb53db613342821cf5f
vivian graeber <h703248488@dhcp-147-4-180-108.dhcp.hofstra.edu>
2023-11-02 14:10:44 -0400

    work

--- csc15/lab06_graeber/graeber_lab6.py
+++ csc15/lab06_graeber/graeber_lab6.py
@@ -1,28 +1,42 @@
 # Author: Vivian Graeber
-# Date: 10/26/23
+# Date: 11/02/23
 # Description: Lab 6 abstraction code
 
+import nltk
 from textblob import TextBlob as tb
+nltk.download("averaged_perceptron_tagger")
+nltk.download("wordnet")
+
+def condenselist(t):
+  nl = []
+  for i in t:
+    if i not in nl:
+      nl.append(i)
+  return nl
 
 # from https://www.geeksforgeeks.org/python-lemmatization-approaches-with-examples/
-def pos_tagger(sentence);
+def numWordVocabs(sentence):
   sent = tb(sentence)
-  tag_dict = {'J': 'a', 'N': 'n', 'V', 'v', 'R': r}
+  tag_dict = {'J': 'a', 'N': 'n', 'V': 'v', 'R': 'r'}
   word_tags = [(w, tag_dict.get(pos[0], 'n')) for w, pos in sent.tags]
   lem_list = [wd.lemmatize(tag) for wd, tag in word_tags]
-  return lem_list
+  return len(condenselist(lem_list))
 
 def numSentences(ui):
   endSent = ['.', '!', '?']
+  titles = ["dr.", "esq.", "hon.", "jr.", "mr.", "mrs.", "ms.", "mx.", "messrs.", "mmes.", "msgr.", "rt.", "sr.", "st.", "ald.", "sen.", "gen.", "rep.", "gov.", "pres.", "col.", "lt.", "insp.", "asst.", "assoc."]
   sentCount = 0
-  for i in range(len(ui)):
-    if ((ui[i] in endSent) and (ui[i - 1] not in endSent)):
+  w = ui.split()
+  for i in w:
+    if ((i[-1] in endSent) and (i[-2] not in endSent) and (i not in titles)):
       sentCount += 1
   return sentCount
 
 def numWordTokens(ui):
-  wc = 1
-  for i in range(len(ui)):
-    if (ui[i] == ' ') and (ui[i - 1] != ' ') and (ui[i] != '.');
-      wc += 1
-  return wc
+  w = ui.split()
+  for i in w:
+    if (i == '—'):
+      w.remove('—')
+    elif (i == "..."):
+      w.remove("...")
+  return len(w)
