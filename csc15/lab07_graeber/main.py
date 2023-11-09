fa47bcf566985b4969d8e7e1cfb93c9feb3112bf
Vivian Graeber <vgraeber@gmail.com>
2023-11-09 12:55:41 -0500

    work

---
+++ csc15/lab07_graeber/main.py
@@ -0,0 +1,36 @@
+# Description Lab 7
+# Create a Bag Of Words Model from speeches
+# Implement function abstraction 
+#
+# Team Members:
+#
+#
+#
+# Revision History
+# -    -    -    -    -    -    -    -    -    -    -
+# name      date        comment
+# scl       11/4/2023   Initial code framework for students
+#
+#
+
+from parseUtility import testFunction
+
+def getFileNames():
+    list_of_files = []
+
+    return list_of_files
+
+def createCorpus():
+    # concat the files to make on text file
+
+    return corpus
+
+
+def main():
+    status = testFunction()
+    print(status)
+
+    return status
+
+# - - run main() - -
+s = main()
