4bc2136e3164853c4844bf492d1ebcae3ab6a5f4
Vivian Graeber <vgraeber@gmail.com>
2024-02-27 04:34:07 -0500

    work

--- csc16/projects/solitaire/main.cpp
+++ csc16/projects/solitaire/main.cpp
@@ -1,165 +1,128 @@
 #include <iostream>
+#include <vector>
+#include <tuple>
 #include <string>
 #include <algorithm>
 #include <random>
 using namespace std;
 
-struct Deck {
-  static const int numcards = 52;
-  static const int numsuits = 4;
-  static const int numranks = 13;
-  string suits[numsuits] = {"S", "H", "C", "D"};
-  string ranks[numranks] = {"A ", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", "10", "J ", "Q ", "K "};
-  string arr[numcards];
-};
-
-struct Header {
-  static const int numcols = 7;
-  static const int numsuits = 4;
-  string newcards[3] = {"***", "---", "   "};
-  string foundation[numsuits] = {"---", "---", "---", "---"};
-  string colnums[numcols] = {" C1", " C2", " C3", " C4", " C5", " C6", " C7"};
-  string div = "~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-};
-
-struct Tableau {
-  static const int numrows = 20;
-  static const int numcols = 7;
-  string arr[numrows][numcols];
-};
-
-Deck carddeck() {
-  Deck cards;
-  for (int s = 0; s < cards.numsuits; s++) {
-    for (int r = 0; r < cards.numranks; r++) {
-      int cnum = s * 13 + r;
-      string card = cards.ranks[r] + cards.suits[s];
-      cards.arr[cnum] = card;
-    }
-  }
-  return cards;
-}
-
-Tableau cardtableau() {
-  Tableau tableau;
-  for (int r = 0; r < tableau.numrows; r++) {
-    for (int c = 0; c < tableau.numcols; c++) {
-      tableau.arr[r][c] = "   ";
-    }
-  }
-  return tableau;
-}
-
-Deck shuffledeck(Deck cards) {
-  random_device rd;
-  mt19937 g(rd());
-  shuffle(begin(cards.arr), end(cards.arr), g);
-  return cards;
-}
-
-string printrules() {
-  cout << "Welcome to vgraeber's version of Klondike Solitaire, v1.0!" << endl;
-  cout << "This game is meant to be played in a command-line interface, a.k.a. a terminal, using keyboard input." << endl;
-  cout << "If you are not playing this game with these, please switch now, as playing in a different environment is untested and may have bugs." << endl << endl;
-  cout << "Note: For both space and display reasons, cards will be displayed in a condensed format. All cards will take up 3 spaces." << endl << "The following is a quick guide to what this shorthand means." << endl << "'***' means a card is face-down" << endl << "'---' means that spot has no cards" << endl << "' 6D', '10S', 'J H', and 'Q C' mean '6 of Diamonds', '10 of Spades', 'Jack of Hearts', and 'Queen of Clubs', respectively. All of the playing cards follow this format." << endl << endl;
-  cout << "How To Play:" << endl << "Type in the name of the card you would like to move, and if there are multiple options for its destination, they will pop up, 'stock' to flip a new card, or 'waste' to move a card from the waste to the tableau." << endl << "Keep going until you either win or can't go any further." << endl << "You may type 'exit' at any time to quit the game." << endl << "If you wish to see these instructions again, just type 'print rules'." << endl;
-  cout << "Type 'continue' to show that you understand the rules and wish to continue." << endl;
-  string uin;
-  do {
-    getline(cin, uin);
-  } while ((uin != "continue") and (uin != "exit"));
-  cout << endl;
-  return uin;
-}
-
-string fullname(string card) {
-  string suit;
-  char cardsuit = card[card.length() - 1];
-  card.pop_back();
-  (cardsuit == 'S') ? (suit = "Spades") : (cardsuit == 'H') ? (suit = "Hearts") : (cardsuit == 'C') ? (suit = "Clubs") : (suit = "Diamonds");
-  string name = card + " of " + suit;
-  return name;
-}
-
-void printheader(Header header) {
-  cout << endl << endl;
-  for (int i = 0; i < 3; i++) {
-    cout << header.newcards[i] << " ";
-  }
-  for (int i = 0; i < header.numsuits; i++) {
-    cout << header.foundation[i] << " ";
-  }
-  cout << endl;
-  for (int i = 0; i < header.numcols; i++) {
-    cout << header.colnums[i] << " ";
-  }
-  cout << endl << header.div << endl;
-}
-
-void printtableau(Tableau tableau) {
-  for (int r = 0; r < tableau.numrows; r++) {
-    for (int c = 0; c < tableau.numcols; c++) {
-      if (r == (tableau.numrows - 1)) {
-        cout << tableau.arr[r][c] << " ";
-      } else if (tableau.arr[r + 1][c] == "   ") {
-        cout << tableau.arr[r][c] << " ";
-      } else {
-        cout << "***" << " ";
+class Deck {
+  public:
+    vector<string> cards = shuffledeck(filldeck());
+    vector<string> filldeck() {
+      string suits[4] = {"S", "H", "C", "D"};
+      string ranks[13] = {"A ", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9", "10", "J ", "Q ", "K "};
+      vector<string> cards;
+      for (int s = 0; s < 4; s++) {
+        for (int r = 0; r < 13; r++) {
+          string card = ranks[r] + suits[s];
+          cards.push_back(card);
+        }
       }
+      return cards;
     }
-    cout << endl;
-  }
-}
-
-void printgame(Header header, Tableau tableau) {
-  printheader(header);
-  printtableau(tableau);
-}
-
-Tableau initgame(Deck cards, Tableau tableau) {
-  for (int r = 0; r < tableau.numcols; r++) {
-    for (int c = r; c < tableau.numcols; c++) {
-      int cardpos = (r * tableau.numcols) + c;
-      tableau.arr[r][c] = cards.arr[cardpos];
+    vector<string> shuffledeck(vector<string> deck) {
+      auto rd = random_device {};
+      auto rng = default_random_engine {rd()};
+      shuffle(deck.begin(), deck.end(), rng);
+      return deck;
     }
-  }
-  return tableau;
-}
+};
 
-Deck remcard(Deck cards, int pos) {
-  cards.arr[pos] = "---";
-  return cards;
-}
-
-string validdestcards(string uin, Tableau tableau) {
-  cout << ' ';
-  return uin;
-}
+class Game {
+  public:
+    inline static const string buffer = "  ";
+    class Header {
+      public:
+        string newcards[3] = {"***", "---", "   "};
+        string foundation[4] = {"---", "---", "---", "---"};
+        string colnums[7] = {" C1", " C2", " C3", " C4", " C5", " C6", " C7"};
+        string divs[7] = {"~~~", "~~~", "~~~", "~~~", "~~~", "~~~", "~~~"};
+        void printheader() {
+          for (int i = 0; i < 3; i++) {
+            cout << newcards[i] << buffer;
+          }
+          for (int i = 0; i < 4; i++) {
+            cout << foundation[i] << buffer;
+          }
+          cout << endl;
+          for (int i = 0; i < 7; i++) {
+            cout << colnums[i] << buffer;
+          }
+          cout << endl;
+          for (int i = 0; i < 7; i++) {
+            cout << divs[i] << buffer;
+          }
+          cout << endl;
+        }
+    };
+    class Body {
+      public:
+        vector<vector<tuple<string, bool, bool>>> tableau = maketableau();
+        vector<vector<tuple<string, bool, bool>>> maketableau() {
+          vector<vector<tuple<string, bool, bool>>> tableau;
+          tableau.resize(7);
+          for (int i = 0; i < tableau.size(); i++) {
+            tableau[i].resize(7);
+            for (int j = 0; j < tableau[i].size(); j++) {
+              tableau[i][j] = make_tuple("   ", true, false);
+            }
+          }
+          return tableau;
+        }
+        int findemptyrowincol(int col, vector<vector<tuple<string, bool, bool>>> tableau) {
+          for (int i = 0; i < tableau.size(); i++) {
+            if (get<2>(tableau[i][col])) {
+              if (get<0>(tableau[i][col]) == "---") {
+                return 0;
+              }
+              return (i + 1);
+            }
+          }
+        }
+        bool checklastrow(vector<vector<tuple<string, bool, bool>>> tableau) {
+          int row = tableau.size() - 1;
+          for (int j = 0; j < tableau[row].size(); j++) {
+            if (get<0>(tableau[row][j]) != "   ") {
+              return false;
+            }
+          }
+          return true;
+        }
+        vector<vector<tuple<string, bool, bool>>> remrow(vector<vector<tuple<string, bool, bool>>> tableau) {
+          int currrows = tableau.size();
+          tableau.resize(currrows - 1);
+          return tableau;
+        }
+        vector<vector<tuple<string, bool, bool>>> addrow(vector<vector<tuple<string, bool, bool>>> tableau) {
+          int currrows = tableau.size();
+          tableau.resize(currrows + 1);
+          tableau[currrows].resize(7);
+          for (int j = 0; j < tableau[currrows].size(); j++) {
+            tableau[currrows][j] = make_tuple("   ", true, false);
+          }
+          return tableau;
+        }
+        vector<vector<tuple<string, bool, bool>>> addcard(int row, int col, string card, vector<vector<tuple<string, bool, bool>>> tableau) {
+          tableau[row][col] = make_tuple(card, true, true);
+          get<2>(tableau[row - 1][col]) = false;
+          return tableau;
+        }
+        void printbody() {
+          for (int i = 0; i < tableau.size(); i++) {
+            for (int j = 0; j < tableau[i].size(); j++) {
+              if (get<1>(tableau[i][j])) {
+                cout << get<0>(tableau[i][j]) << buffer;
+              } else {
+                cout << "***" << buffer;
+              }
+            }
+          }
+        }
+    };
+};
 
 int main() {
-  Deck cards = carddeck();
-  Header header;
-  Tableau tableau = cardtableau();
-  cards = shuffledeck(cards);
-  tableau = initgame(cards, tableau);
-  for (int i = 0; i < pow(tableau.numcols, 2); i++) {
-    cards = remcard(cards, i);
-  }
-  string uin = printrules();
-  while (uin != "exit") {
-    printgame(header, tableau);
-    getline(cin, uin);
-    if (uin == "exit") {
-      cout << "this ran";
-    } else if (uin == "stock") {
-      cout << ' ';
-    } else if (uin == "waste") {
-      cout << ' ';
-    } else if (uin == "print rules") {
-      printrules();
-    } else {
-      string validdests = validdestcards(uin, tableau);
-    }
-  }
+  Game game;
   return 0;
 }
