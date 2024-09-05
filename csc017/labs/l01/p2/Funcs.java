9b1efd86eb980d68954222005cf484597de7cde0
Vivian Graeber <vgraeber@gmail.com>
2024-09-05 17:08:21 -0400

    work

--- csc017/labs/l01/p2/Funcs.java
+++ csc017/labs/l01/p2/Funcs.java
@@ -15,9 +15,12 @@ public class Funcs {
     return sum;
   }
   public static String smallest(String[] strArr) {
-    String smallest = null;
+    if (strArr.length == 0) {
+      return null;
+    }
+    String smallest = strArr[0];
     for (int i = 0; i < strArr.length; i++) {
-      if (smallest.compareTo(strArr[i]) < 0) {
+      if (smallest.compareTo(strArr[i]) > 0) {
         smallest = strArr[i];
       }
     }
@@ -37,9 +40,9 @@ public class Funcs {
     }
     return false;
   }
-  public static boolean duplicates(String[] strArr) {
+  public static boolean duplicate(String[] strArr) {
     for (int i = 0; i < strArr.length; i++) {
-      for (int j = i = 1; j < strArr.length; j++) {
+      for (int j = i + 1; j < strArr.length; j++) {
         if (strArr[i].equals(strArr[j])) {
           return true;
         }
@@ -47,7 +50,138 @@ public class Funcs {
     }
     return false;
   }
+  public static void printQ1(String testStr, String[] inputStrs) {
+    System.out.print("the string \"");
+    System.out.print(testStr);
+    System.out.print("\" is ");
+    if (!inArr(testStr, inputStrs)) {
+      System.out.print("not ");
+    }
+    System.out.print("in the array of strings ");
+    for (int i = 0; i < inputStrs.length - 1; i++) {
+      System.out.print("\"");
+      System.out.print(inputStrs[i]);
+      System.out.print("\", ");
+    }
+    System.out.print("and \"");
+    System.out.print(inputStrs[inputStrs.length - 1]);
+    System.out.println("\"");
+  }
+  public static void printQ2A(int[] inputInts) {
+    System.out.print("the sum of the integers ");
+    for (int i = 0; i < inputInts.length - 1; i++) {
+      System.out.print(inputInts[i]);
+      System.out.print(", ");
+    }
+    System.out.print("and ");
+    System.out.print(inputInts[inputInts.length - 1]);
+    System.out.print(" is ");
+    System.out.println(sum(inputInts));
+  }
+  public static void printQ2B(String[] inputStrs) {
+    System.out.print("the smallest string in the array of strings ");
+    for (int i = 0; i < inputStrs.length - 1; i++) {
+      System.out.print("\"");
+      System.out.print(inputStrs[i]);
+      System.out.print("\", ");
+    }
+    System.out.print("and \"");
+    System.out.print(inputStrs[inputStrs.length - 1]);
+    System.out.print("\" is \"");
+    System.out.print(smallest(inputStrs));
+    System.out.println("\"");
+  }
+  public static void printQ3A(String[] inputStrs) {
+    System.out.print("the array of strings ");
+    for (int i = 0; i < inputStrs.length - 1; i++) {
+      System.out.print("\"");
+      System.out.print(inputStrs[i]);
+      System.out.print("\", ");
+    }
+    System.out.print("and \"");
+    System.out.print(inputStrs[inputStrs.length - 1]);
+    System.out.print("\" in reverse is ");
+    for (int i = 0; i < inputStrs.length - 1; i++) {
+      System.out.print("\"");
+      System.out.print(reverse(inputStrs[i]));
+      System.out.print("\", ");
+    }
+    System.out.print("and \"");
+    System.out.print(reverse(inputStrs[inputStrs.length - 1]));
+    System.out.println("\"");
+  }
+  public static void printQ3B(String[] inputStrs) {
+    System.out.print("the palindromes in the array of strings ");
+    for (int i = 0; i < inputStrs.length - 1; i++) {
+      System.out.print("\"");
+      System.out.print(inputStrs[i]);
+      System.out.print("\", ");
+    }
+    System.out.print("and \"");
+    System.out.print(inputStrs[inputStrs.length - 1]);
+    System.out.print("\"");
+    int palindromes = 0;
+    for (int i = 0; i < inputStrs.length; i++) {
+      if (palindrome(inputStrs[i])) {
+        palindromes += 1;
+      }
+    }
+    System.out.print(" are ");
+    if (palindromes == 0) {
+      System.out.println(" nonexistent");
+    } else if (palindromes == 1) {
+      for (int i = 0; i < inputStrs.length; i++) {
+        if (palindrome(inputStrs[i])) {
+          System.out.print("\"");
+          System.out.print(inputStrs[i]);
+          System.out.println("\"");
+        }
+      }
+    } else {
+      int i = 0;
+      while (palindromes > 0) {
+        if (palindrome(inputStrs[i])) {
+          System.out.print("\"");
+          System.out.print(inputStrs[i]);
+          System.out.print("\"");
+          palindromes -= 1;
+          if (palindromes > 0) {
+            System.out.print(", ");
+          }
+          if (palindromes == 1) {
+            System.out.print("and ");
+          }
+        }
+        i += 1;
+      }
+      System.out.println();
+    }
+  }
+  public static void printQ4(String[] inputStrs) {
+    System.out.print("the array of strings ");
+    for (int i = 0; i < inputStrs.length - 1; i++) {
+      System.out.print("\"");
+      System.out.print(inputStrs[i]);
+      System.out.print("\", ");
+    }
+    System.out.print("and \"");
+    System.out.print(inputStrs[inputStrs.length - 1]);
+    System.out.print("\"");
+    if (duplicate(inputStrs)) {
+      System.out.println(" contains duplicates");
+    } else {
+      System.out.println(" does not contain duplicates");
+    }
+  }
   public static void main(String[] args) {
-    //demo funcs here
+    String[] inputStrs = {"a", "abba", "dancing", "queen", "queen", "bohemian", "raphsody"};
+    int[] inputInts = {1, 4, 7, 5, 5, 8, 8};
+    String testStr = "music";
+    printQ1(testStr, inputStrs);
+    printQ2A(inputInts);
+    printQ2B(inputStrs);
+    printQ3A(inputStrs);
+    printQ3B(inputStrs);
+    printQ4(inputStrs);
   }
 }
--- csc017/labs/l01/p2/Team.java
+++ csc017/labs/l01/p2/Team.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Team {
   double winRate;
   int wins;
@@ -7,12 +9,11 @@ public class Team {
   public Team(String teamName) {
     wins = 0;
     losses = 0;
-    draws = 0;
     winRate = 0;
     name = teamName;
   }
   public void calcWinRate() {
-    double totGames = 0.0 + wins + losses + draws;
+    double totGames = 0.0 + wins + losses;
     winRate = wins / totGames;
   }
   public void win() {
@@ -23,51 +24,78 @@ public class Team {
     losses += 1;
     calcWinRate();
   }
-  public void draw() {
-    draws += 1;
-    calcWinRate();
-  }
   public void printRecord() {
     System.out.print("W-L-D: ");
     System.out.print(wins);
     System.out.print("-");
     System.out.print(losses);
-    System.out.print("-");
-    System.out.println(draws);
   }
   public void play(Team other) {
-    int n1 = (int)(Math.random() * 100);
-    int n2 = (int)(Math.random() * 100);
-    if (n1 > n2) {
+    Random randGen = new Random();
+    double randNum = randGen.nextGaussian();
+    double rNum1 = randNum * (1 + this.losses) + this.winRate;
+    double rNum2 = randNum * (1 + other.losses) + other.winRate;
+    if (rNum1 > rNum2) {
       this.win();
       other.lose();
-      System.out.print(this.name);
-      System.out.println(" win");
-    } else if (n1 < n2) {
+      //System.out.print(this.name);
+      //System.out.println(" win");
+    } else {
       this.lose();
       other.win();
-      System.out.print(other.name);
-      System.out.println(" win");
-    } else {
-      this.draw();
-      other.draw();
-      System.out.println("It's a draw");
+      //System.out.print(other.name);
+      //System.out.println(" win");
     }
   }
-  public static void sortnames(String[] names) {
-    sort(names);
-    System.out.println(names);
+  public static Team[] makeTeams(String[] teamNames) {
+    Team[] teams = new Team[teamNames.length];
+    for (int i = 0; i < teamNames.length; i++) {
+      teams[i] = new Team(teamNames[i]);
+    }
+    return teams;
+  }
+  public static void season(Team[] teams) {
+    for (int i = 0; i < teams.length; i++) {
+      for (int j = i + 1; j < teams.length; j++) {
+        teams[i].play(teams[j]);
+      }
+    }
+  }
+  public static void seasonResults(Team[] teams) {
+    Team[] orderedTeams = teams.clone();
+    for (int i = 0; i < teams.length; i++) {
+      for (int j = i + 1; j < teams.length; j++) {
+        if (orderedTeams[i].winRate < orderedTeams[j].winRate) {
+          Team temp = orderedTeams[i];
+          orderedTeams[i] = orderedTeams[j];
+          orderedTeams[j] = temp;
+        }
+      }
+    }
+    for (int i = 0; i < teams.length; i++) {
+      System.out.print(orderedTeams[i].name);
+      System.out.print(" W-L: ");
+      System.out.print(orderedTeams[i].wins);
+      System.out.print("-");
+      System.out.print(orderedTeams[i].losses);
+      System.out.print(" win rate: ");
+      System.out.println(orderedTeams[i].winRate);
+    }
   }
   public static void main(String[] args) {
+    /*
     Team t1 = new Team("giants");
     Team t2 = new Team("jets");
     t1.lose();
     t2.win();
     t2.lose();
-    t2.printRecord(); // should print "W-L: 1-1"
-    t1.play(t2); // should print "giants win" or "jets win"
-    System.out.println(t1.winRate); // prints .000 to 1.00
-    String[] teamNames = {"Giants", "Jets", "Rams", "Patriots", "Falcons", "Steelers", "Packers", "Eagles", "Chiefs", "Bills", "Seahawks", "Cowboys", "Chargers", "Raiders", "Dolphins", "Saints", "49ers", "Broncos"};
-    sortnames(teamNames);
+    t2.printRecord();
+    t1.play(t2);
+    System.out.println(t1.winRate);
+    */
+    String[] teamNames = {"49ers", "Bills", "Broncos", "Chargers", "Chiefs", "Cowboys", "Dolphins", "Eagles", "Falcons", "Giants", "Jets", "Packers", "Patriots", "Raiders", "Rams", "Saints", "Seahawks", "Steelers"};
+    Team[] teams = makeTeams(teamNames);
+    season(teams);
+    seasonResults(teams);
   }
 }
