1d937d6ff77d58ad423d6f465575561420bb97dd
Vivian Graeber <vgraeber@gmail.com>
2024-09-10 17:02:07 -0400

    work

--- csc017/labs/l02/maze.java
+++ csc017/labs/l02/maze.java
@@ -3,10 +3,9 @@ public class maze extends mazebase {
   static int[] dirs = {0, 1, 2, 3};
   static int[] rowChange = {-1, 1, 0, 0};
   static int[] colChange = {0, 0, 1, -1};
-
-  public maze() {
+  /*public maze() {
     super();
-  }
+  }*/
   public void shuffleDirs(int[] dirs) {
     for (int i = dirs.length - 1; i > 0; i--) {
       int j = (int)(Math.random() * (i + 1));
@@ -16,23 +15,16 @@ public class maze extends mazebase {
     }
   }
   public boolean inBounds(int row, int col) {
-    if (((0 < row) && (row < mwidth)) && ((0 < col) && (col < mheight))) {
+    if (((0 < row) && (row < mwidth - 1)) && ((0 < col) && (col < mheight - 1))) {
       return true;
     }
     return false;
   }
   @Override
   public void digout(int row, int col) {
-    // The following is a skeleton program that demonstrates the mechanics
-    // needed for the completion of the program.
-    // We always dig out two spaces at a time: we look two spaces ahead
-    // in the direction we're trying to dig out, and if that space has
-    // not already been dug out, we dig out that space as well as the
-    // intermediate space.  This makes sure that there's always a wall
-    // separating adjacent corridors.
-    M[row][col] = 1;  // digout maze at coordinate y,x
-    drawblock(row, col);  // change graphical display to reflect space dug out
-    nextframe(40); // show next animation frame after 40ms delay
+    M[row][col] = 1;
+    drawblock(row, col);
+    nextframe(40);
     shuffleDirs(dirs);
     int[] currDirs = dirs.clone();
     for (int i = 0; i < currDirs.length; i++) {
@@ -49,11 +41,12 @@ public class maze extends mazebase {
     }
   }
   public static void main(String[] av) {
-	  new maze(); // constructor of superclass will initiate everything
+	  new maze();
   }
   // other hints:  override customize to change maze parameters:
   @Override
   public void customize() {
-	// ... can change mwidth, mheight, bw,bh, colors here
+    mwidth = 41;
+    mheight = 41;
   }
 }
